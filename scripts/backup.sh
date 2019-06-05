d885330c225533ccbdc01f8e49a833884553ce48
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-06-05 14:33:13 +0200

    Fix backup scripts for configs with multiple databases

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,9 +9,9 @@ if [ -z "$SKIP_BACKUP" ]; then
   do
     for j in $(echo "1,2,3,4,5,6" | sed "s/,/ /g")
     do
-      TABLECOUNT=`echo "show tables;" | mysql $MYSQL_DATABASE | wc | awk '{print $1}'`
+      TABLECOUNT=`echo "show tables;" | mysql $i | wc | awk '{print $1}'`
       if [ $TABLECOUNT == "0" ]; then
-        echo "Waiting for restore... ($j/6)"
+        echo "Waiting for mysql to startup... ($j/6)"
         sleep 10
       fi
     done
